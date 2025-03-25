9b71e242b084b4d72778f884388e1a193fe6807b
Alizarbukhari <141038313+Alizarbukhari@users.noreply.github.com>
2025-03-25 23:27:03 +0500

    Work Page

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,9 +1,13 @@
+import { Work_Sans } from 'next/font/google'
 import React from 'react'
+import WorkHeader from './Components/workHeader'
 
 export default function Work
 () {
   return (
-    <div>Work
+    <div>
+      
+  <WorkHeader/>
         
     </div>
   )
