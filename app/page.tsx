061a4fe2f6f91042909ab938553b021b84e84df3
devmianharoon <devmianharoon@gmail.com>
2025-03-27 00:44:14 +0500

    socail content

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,71 @@
 import Image from "next/image";
-
+import Link from "next/link";
+import { socicalData } from "../data/utils/socialData";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <section>
+      {/* Main Container */}
+      <div className="flex flex-col w-full px-8 pt-[16rem] pb-[8rem]">
+        {/* Social Section */}
+        <div className="w-full  flex">
+          <div className="flex flex-1 justify-start items-start max-w-[50%]">
+            <h1 className="z-[2] tracking-[.2rem] uppercase mix-blend-exclusion text-[4.2rem] font-normal leading-[1.05]">
+              Social
+            </h1>
+          </div>
+          <div className="flex flex-col flex-1 justify-start items-start max-w-[50%]">
+            <p className="text-normal leading-[1.65] max-w-[42rem] text-[.9rem] font-thin">
+              We craft compelling content that resonates across all platforms,
+              sparking engagement and driving meaningful conversations. By
+              leveraging our deep understanding of the landscape, we create
+              campaigns that stand out amongst the vast ocean of social media
+              content. Whether it’s finding a viral moment or helping you
+              sustain your brand story, our social campaigns are designed to cut
+              through the noise, amplify your message, and build a loyal
+              community around your brand.
+            </p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+        {/* Profile Section */}
+        <div className="w-full pt-[6rem] pb-[4rem ">
+          <div className="grid gap-x-[2rem] gap-y-[4rem] grid-rows-auto grid-cols-[repeat(auto-fit,_minmax(16rem,_1fr))]">
+          {socicalData.map((social) => (
+            <div
+              key={social.id}
+              className=""
+            >
+              {/* Componnt */}
+              <Link
+                className="text-white text-center text-ellipsis flex flex-col justify-start items-center relative overflow-hidden"
+                href={`https://www.instagram.com/${social.instaName}`}
+              >
+                <div className="z-[12] aspect-[1] rounded-full w-[10rem] relative overflow-hidden transform translate-x-0 translate-y-0 translate-z-0 scale-x-100 scale-y-100 rotate-x-0 rotate-y-0 rotate-z-0 skew-x-0 skew-y-0">
+                  <Image
+                    src={social.image}
+                    alt="media"
+                    layout="fill"
+                    className=""
+                  />
+                </div>
+                <div className="z-[12] mt-3 p-2 px-4 relative overflow-hidden">
+                  <div className="transform  scale-x-100 scale-y-100 rotate-x-0 rotate-y-0 rotate-z-0 skew-x-0 skew-y-0">
+                    <div className="flex flex-col justify-start items-center">
+                      <div className="tracking-[.1rem] uppercase max-w-[70rem] text-[1.75rem] font-normal leading-[1.15]">
+                        {social.title}
+                      </div>
+                      <div className="uppercase max-w-[42rem] text-[.9rem] font-thin">
+                        @{social.instaName}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
