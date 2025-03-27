c62f87f55104c316f09765acde72c1c4e6daeee9
Hamadalishah <hammadalis459@gmail.com>
2025-03-28 00:39:28 +0500

    make the some changes

--- app/work/Components/sliderImages.tsx
+++ app/work/Components/sliderImages.tsx
@@ -1,16 +1,11 @@
-'use client';
+'use client'
 import React, { useRef } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules';
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 import { socialText, socialSlid, socialSlid2, socialSlid3, socialSlid4, socialSlid5 } from '../../../data/utils/workslidedata';
 import Image from 'next/image';
-
-// Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/navigation';
 
-// Array of all slide datasets
 const allSlides = [socialSlid, socialSlid2, socialSlid3, socialSlid4, socialSlid5];
 
 export default function SliderImages() {
@@ -27,14 +22,61 @@ export default function SliderImages() {
   );
 }
 
+interface Slide {
+  id: number;
+  img: string;
+}
+
 interface SliderSectionProps {
   title: string;
-  slides: Array<{ id: number; img: string }>;
+  slides: Slide[];
 }
 
 function SliderSection({ title, slides }: SliderSectionProps) {
-  const prevRef = useRef<HTMLDivElement>(null);
-  const nextRef = useRef<HTMLDivElement>(null);
+  const swiperRef = useRef<any>(null);
+
+  // Process slides to ensure total count is a multiple of 6
+  const getProcessedSlides = (): Slide[] => {
+    let arr = [...slides];
+    const remainder = arr.length % 6;
+    if (remainder !== 0) {
+      const extra = 6 - remainder;
+      arr = arr.concat(arr.slice(0, extra));
+    }
+    return arr;
+  };
+
+  // Base slides that are multiple of 6
+  const baseSlides = getProcessedSlides();
+  const total = baseSlides.length;
+
+  // Duplicate slides three times to simulate infinite scrolling
+  const infiniteSlides = [...baseSlides, ...baseSlides, ...baseSlides];
+
+  // Navigation: always slide in the same direction
+  const handlePrev = () => {
+    const swiper = swiperRef.current.swiper;
+    let newIndex = swiper.activeIndex - 6;
+    swiper.slideTo(newIndex, 600);
+    // If index goes before the first copy, reset to the equivalent index in the middle copy without animation
+    if (newIndex < total) {
+      swiper.once('transitionEnd', () => {
+        swiper.slideTo(newIndex + total, 0);
+      });
+    }
+  };
+
+  const handleNext = () => {
+    const swiper = swiperRef.current.swiper;
+    let newIndex = swiper.activeIndex + 6;
+    swiper.slideTo(newIndex, 600);
+    // If index exceeds the middle copy, reset back to middle copy without animation
+    if (newIndex >= total * 2) {
+      swiper.once('transitionEnd', () => {
+        swiper.slideTo(newIndex - total, 0);
+      });
+    }
+  };
 
   return (
     <section className='mb-2'>
@@ -42,53 +84,36 @@ function SliderSection({ title, slides }: SliderSectionProps) {
         <h1 className='text-[20px] font-sans text-red-50'>{title}</h1>
       </div>
       
-      <div className="slider-container bg-[#000000] "
-        style={{
-          position: 'relative',
-          width: '100%',
-          margin: '0 auto',
-          overflow: 'hidden',
-        }}
-      >
-        {/* Navigation Arrows */}
-        <div ref={prevRef} className="arrow-prev absolute transition-opacity duration-300 ease-in-out hover:scale-125  top-6 -translate-y-1/2 left-4 z-20 text-white cursor-pointer">
+      <div className="slider-container bg-[#000000] relative w-full mx-auto overflow-hidden">
+        {/* Custom Navigation Arrows */}
+        <div onClick={handlePrev} className="arrow-prev absolute transition-opacity duration-300 ease-in-out hover:scale-125 top-6 -translate-y-1/2 left-4 z-20 text-white cursor-pointer">
           <MdKeyboardArrowLeft size={60} />
         </div>
-        <div ref={nextRef} className="arrow-next absolute transition-opacity duration-300 ease-in-out hover:scale-125 top-6 -translate-y-1/2 right-1 z-20 text-white cursor-pointer">
+        <div onClick={handleNext} className="arrow-next absolute transition-opacity duration-300 ease-in-out hover:scale-125 top-6 -translate-y-1/2 right-1 z-20 text-white cursor-pointer">
           <MdKeyboardArrowRight size={60} />
         </div>
 
         <Swiper
-          modules={[Navigation]}
-          navigation={{
-            prevEl: prevRef.current,
-            nextEl: nextRef.current,
-          }}
-          onBeforeInit={(swiper) => {
-            // @ts-ignore
-            swiper.params.navigation.prevEl = prevRef.current;
-            // @ts-ignore
-            swiper.params.navigation.nextEl = nextRef.current;
-          }}
-          loop={true}
-          centeredSlides={true}
-          slidesPerView={6} // Ya fixed value like 4
-          spaceBetween={10}
+          ref={swiperRef}
+          initialSlide={total} // Start from the middle copy
+          loop={false}     // Loop is handled manually
+          slidesPerView={6}
+          spaceBetween={5}
           className="h-full shadow-none brightness-100 flex"
         >
-          {slides.map((item) => (
+          {infiniteSlides.map((item, index) => (
             <SwiperSlide 
-              key={item.id}
+              key={index} // index used as key since duplicates exist
               className="h-[124px] flex items-center justify-center shadow-none"
             >
-              <div className="relative w-full h-full flex  transition-all duration-300 hover:scale-105 ">
-                <div className=" w-[221px] h-[124px] ">
+              <div className="relative w-full h-full flex transition-all duration-300 hover:scale-105">
+                <div className="w-[250px] h-[124px]">
                   <Image
                     src={item.img}
                     alt={`Slide ${item.id}`}
-                    width={221}
+                    width={250}
                     height={124}
-                    className=" w-[221px] h-[124px] flex   "
+                    className="w-[250px] h-[124px] flex"
                     priority
                   />
                 </div>
---
+++ data/utils/socailslidedata2.ts
@@ -0,0 +1,182 @@
+
+export const socialSlid = [{
+    id: 1,
+    img: "/worksImages/work_social_images/s12.jpg"
+},
+{
+    id:2,
+    img:'/worksImages/work_social_images/s13.jpg'
+},
+{
+    id:3,
+    img:'/worksImages/work_social_images/s14.jpg'
+},{
+    id:4,
+    img:'/worksImages/work_social_images/s15.jpg'
+},{
+    id:5,
+    img:'/worksImages/work_social_images/s18.jpg'
+},{
+    id:6,
+    img:'/worksImages/work_social_images/s6.jpg'
+},{
+    id:7,
+    img:'/worksImages/work_social_images/s7.jpg'
+},{
+    id:8,
+    img:'/worksImages/work_social_images/s19.jpg'
+},{
+    id:9,
+    img:'/worksImages/work_social_images/s9.jpg'
+},{
+    id:10,
+    img:'/worksImages/work_social_images/s120.jpg'
+},{
+    id:11,
+    img:'/worksImages/work_social_images/s121.jpg'
+}]
+
+export const socialSlid2= [{
+    id: 1,
+    img: "/worksImages/work_social_images/s12.jpg"
+},
+{
+    id:2,
+    img:'/worksImages/work_social_images/s2.jpg'
+},
+{
+    id:3,
+    img:'/worksImages/work_social_images/s14.jpg'
+},{
+    id:4,
+    img:'/worksImages/work_social_images/s15.jpg'
+},{
+    id:5,
+    img:'/worksImages/work_social_images/s16.jpg'
+},{
+    id:6,
+    img:'/worksImages/work_social_images/s6.jpg'
+},{
+    id:7,
+    img:'/worksImages/work_social_images/s7.jpg'
+},{
+    id:8,
+    img:'/worksImages/work_social_images/s19.jpg'
+},{
+    id:9,
+    img:'/worksImages/work_social_images/s9.jpg'
+},{
+    id:10,
+    img:'/worksImages/work_social_images/s120.jpg'
+},{
+    id:11,
+    img:'/worksImages/work_social_images/s121.jpg'
+}]
+export const socialSlid3 = [{
+    id: 1,
+    img: "/worksImages/work_social_images/s12.jpg"
+},
+{
+    id:2,
+    img:'/worksImages/work_social_images/s2.jpg'
+},
+{
+    id:3,
+    img:'/worksImages/work_social_images/s14.jpg'
+},{
+    id:4,
+    img:'/worksImages/work_social_images/s15.jpg'
+},{
+    id:5,
+    img:'/worksImages/work_social_images/s18.jpg'
+},{
+    id:6,
+    img:'/worksImages/work_social_images/s17.jpg'
+},{
+    id:7,
+    img:'/worksImages/work_social_images/s7.jpg'
+},{
+    id:8,
+    img:'/worksImages/work_social_images/s19.jpg'
+},{
+    id:9,
+    img:'/worksImages/work_social_images/s9.jpg'
+},{
+    id:10,
+    img:'/worksImages/work_social_images/s120.jpg'
+},{
+    id:11,
+    img:'/worksImages/work_social_images/s121.jpg'
+}] 
+export const socialSlid4= [{
+    id: 1,
+    img: "/worksImages/work_social_images/s12.jpg"
+},
+{
+    id:2,
+    img:'/worksImages/work_social_images/s2.jpg'
+},
+{
+    id:3,
+    img:'/worksImages/work_social_images/s14.jpg'
+},{
+    id:4,
+    img:'/worksImages/work_social_images/s15.jpg'
+},{
+    id:5,
+    img:'/worksImages/work_social_images/s18.jpg'
+},{
+    id:6,
+    img:'/worksImages/work_social_images/s6.jpg'
+},{
+    id:7,
+    img:'/worksImages/work_social_images/s18.jpg'
+},{
+    id:8,
+    img:'/worksImages/work_social_images/s19.jpg'
+},{
+    id:9,
+    img:'/worksImages/work_social_images/s9.jpg'
+},{
+    id:10,
+    img:'/worksImages/work_social_images/s120.jpg'
+},{
+    id:11,
+    img:'/worksImages/work_social_images/s121.jpg'
+}]
+export const socialSlid5 = [{
+    id: 1,
+    img: "/worksImages/work_social_images/s12.jpg"
+},
+{
+    id:2,
+    img:'/worksImages/work_social_images/s2.jpg'
+},
+{
+    id:3,
+    img:'/worksImages/work_social_images/s14.jpg'
+},{
+    id:4,
+    img:'/worksImages/work_social_images/s15.jpg'
+},{
+    id:5,
+    img:'/worksImages/work_social_images/s18.jpg'
+},{
+    id:6,
+    img:'/worksImages/work_social_images/s6.jpg'
+},{
+    id:7,
+    img:'/worksImages/work_social_images/s7.jpg'
+},{
+    id:8,
+    img:'/worksImages/work_social_images/ss19.jpg'
+},{
+    id:9,
+    img:'/worksImages/work_social_images/s9.jpg'
+},{
+    id:10,
+    img:'/worksImages/work_social_images/s12.jpg'
+},{
+    id:11,
+    img:'/worksImages/work_social_images/s12.jpg'
+}]
