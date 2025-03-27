d7e2c88610e3764eb275b2c160537ab49afa17eb
Alizarbukhari <141038313+Alizarbukhari@users.noreply.github.com>
2025-03-27 23:35:49 +0500

    work on work component

--- app/work/Components/sliderImages.tsx
+++ app/work/Components/sliderImages.tsx
@@ -3,81 +3,100 @@ import React, { useRef } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
-import { socialSlid } from '../../../data/utils/workslidedata';
+import { socialText, socialSlid, socialSlid2, socialSlid3, socialSlid4, socialSlid5 } from '../../../data/utils/workslidedata';
 import Image from 'next/image';
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 
+// Array of all slide datasets
+const allSlides = [socialSlid, socialSlid2, socialSlid3, socialSlid4, socialSlid5];
+
 export default function SliderImages() {
+  return (
+    <div className='w-full'>
+      {socialText.map((textItem, index) => (
+        <SliderSection 
+          key={textItem.id}
+          title={textItem.text}
+          slides={allSlides[index]}
+        />
+      ))}
+    </div>
+  );
+}
+
+interface SliderSectionProps {
+  title: string;
+  slides: Array<{ id: number; img: string }>;
+}
+
+function SliderSection({ title, slides }: SliderSectionProps) {
   const prevRef = useRef<HTMLDivElement>(null);
   const nextRef = useRef<HTMLDivElement>(null);
 
   return (
-    <div className='h-[300px] w-full'>  
-    <div className=' mb-6 ml-6'> 
-    <h1 className=' text-[20px] font-sans text-red-50'> SURF </h1>
-
-    </div>
-      <div
-      className="slider-container bg-[#000000] h-[200px] " // Fixed container height
-      style={{
-        position: 'relative',
-        width: '100%',
-        margin: '0 auto',
-        overflow: 'hidden',
-      }}
-    >
-      {/* Custom Navigation Buttons */}
-      <div ref={prevRef} className="arrow-prev absolute top-2-translate-y-1/2 left-4 z-20 text-white cursor-pointer">
-        <MdKeyboardArrowLeft size={60} />
+    <section className='mb-2'>
+      <div className='mb-2 ml-6'> 
+        <h1 className='text-[20px] font-sans text-red-50'>{title}</h1>
       </div>
-      <div ref={nextRef} className="arrow-next absolute top-8 -translate-y-1/2 right-4 z-20 text-white cursor-pointer">
-        <MdKeyboardArrowRight size={60} />
-      </div>
-
-      <Swiper
-        modules={[Navigation]}
-        navigation={{
-          prevEl: prevRef.current,
-          nextEl: nextRef.current,
+      
+      <div className="slider-container bg-[#000000] "
+        style={{
+          position: 'relative',
+          width: '100%',
+          margin: '0 auto',
+          overflow: 'hidden',
         }}
-        onBeforeInit={(swiper) => {
-          // @ts-ignore
-          swiper.params.navigation.prevEl = prevRef.current;
-          // @ts-ignore
-          swiper.params.navigation.nextEl = nextRef.current;
-        }}
-        loop={true}
-        centeredSlides={true}
-        slidesPerView={5}
-        spaceBetween={30} // Add spacing between slides
-        direction="horizontal"
-        className="h-full" // Full height for Swiper
       >
-        {socialSlid.map((item) => (
-          <SwiperSlide 
-            key={item.id}
-            className="h-[124px] flex items-center justify-center" // Fixed slide height
-          >
-            <div className="relative w-full h-full transition-opacity duration-300 ease-in-out hover:opacity-75"
-              style={{ willChange: 'opacity' }}
->  <div className="absolute inset-0 overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300">
-              <Image
-                src={item.img}
-                alt={`Slide ${item.id}`}
-                layout="fill"
-                className=" w-[300px] h-[124px]" // Maintain aspect ratio
-                priority
-              />
-            </div>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
-    </div>
+        {/* Navigation Arrows */}
+        <div ref={prevRef} className="arrow-prev absolute transition-opacity duration-300 ease-in-out hover:scale-125  top-6 -translate-y-1/2 left-4 z-20 text-white cursor-pointer">
+          <MdKeyboardArrowLeft size={60} />
+        </div>
+        <div ref={nextRef} className="arrow-next absolute transition-opacity duration-300 ease-in-out hover:scale-125 top-6 -translate-y-1/2 right-1 z-20 text-white cursor-pointer">
+          <MdKeyboardArrowRight size={60} />
+        </div>
 
+        <Swiper
+          modules={[Navigation]}
+          navigation={{
+            prevEl: prevRef.current,
+            nextEl: nextRef.current,
+          }}
+          onBeforeInit={(swiper) => {
+            // @ts-ignore
+            swiper.params.navigation.prevEl = prevRef.current;
+            // @ts-ignore
+            swiper.params.navigation.nextEl = nextRef.current;
+          }}
+          loop={true}
+          centeredSlides={true}
+          slidesPerView={6} // Ya fixed value like 4
+          spaceBetween={10}
+          className="h-full shadow-none brightness-100 flex"
+        >
+          {slides.map((item) => (
+            <SwiperSlide 
+              key={item.id}
+              className="h-[124px] flex items-center justify-center shadow-none"
+            >
+              <div className="relative w-full h-full flex  transition-all duration-300 hover:scale-105 ">
+                <div className=" w-[221px] h-[124px] ">
+                  <Image
+                    src={item.img}
+                    alt={`Slide ${item.id}`}
+                    width={221}
+                    height={124}
+                    className=" w-[221px] h-[124px] flex   "
+                    priority
+                  />
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
   );
 }
--- data/utils/workslidedata.ts
+++ data/utils/workslidedata.ts
@@ -20,9 +20,175 @@ export const slideImg = [
 ]
 
 
+export const socialText = [
+    {id:1,
+    text:'SURF'
+    },
+    {
+        id:2,
+        text:"Skateboarding"
+    },
+    {
+        id:3,
+        text:"People"
+    },
+    {
+        id:4,
+        text:"Team Average"
+    },
+    {
+        id:5,
+        text:"Table Talk Series"
+    }
+] 
 
 
 export const socialSlid = [{
+    id: 1,
+    img: "/worksImages/work_social_images/s1.jpg"
+},
+{
+    id:2,
+    img:'/worksImages/work_social_images/s2.jpg'
+},
+{
+    id:3,
+    img:'/worksImages/work_social_images/s3.jpg'
+},{
+    id:4,
+    img:'/worksImages/work_social_images/s4.jpg'
+},{
+    id:5,
+    img:'/worksImages/work_social_images/s5.jpg'
+},{
+    id:6,
+    img:'/worksImages/work_social_images/s6.jpg'
+},{
+    id:7,
+    img:'/worksImages/work_social_images/s7.jpg'
+},{
+    id:8,
+    img:'/worksImages/work_social_images/s8.jpg'
+},{
+    id:9,
+    img:'/worksImages/work_social_images/s9.jpg'
+},{
+    id:10,
+    img:'/worksImages/work_social_images/s10.jpg'
+},{
+    id:11,
+    img:'/worksImages/work_social_images/s11.jpg'
+}]
+
+export const socialSlid2= [{
+    id: 1,
+    img: "/worksImages/work_social_images/s1.jpg"
+},
+{
+    id:2,
+    img:'/worksImages/work_social_images/s2.jpg'
+},
+{
+    id:3,
+    img:'/worksImages/work_social_images/s3.jpg'
+},{
+    id:4,
+    img:'/worksImages/work_social_images/s4.jpg'
+},{
+    id:5,
+    img:'/worksImages/work_social_images/s5.jpg'
+},{
+    id:6,
+    img:'/worksImages/work_social_images/s6.jpg'
+},{
+    id:7,
+    img:'/worksImages/work_social_images/s7.jpg'
+},{
+    id:8,
+    img:'/worksImages/work_social_images/s8.jpg'
+},{
+    id:9,
+    img:'/worksImages/work_social_images/s9.jpg'
+},{
+    id:10,
+    img:'/worksImages/work_social_images/s10.jpg'
+},{
+    id:11,
+    img:'/worksImages/work_social_images/s11.jpg'
+}]
+export const socialSlid3 = [{
+    id: 1,
+    img: "/worksImages/work_social_images/s1.jpg"
+},
+{
+    id:2,
+    img:'/worksImages/work_social_images/s2.jpg'
+},
+{
+    id:3,
+    img:'/worksImages/work_social_images/s3.jpg'
+},{
+    id:4,
+    img:'/worksImages/work_social_images/s4.jpg'
+},{
+    id:5,
+    img:'/worksImages/work_social_images/s5.jpg'
+},{
+    id:6,
+    img:'/worksImages/work_social_images/s6.jpg'
+},{
+    id:7,
+    img:'/worksImages/work_social_images/s7.jpg'
+},{
+    id:8,
+    img:'/worksImages/work_social_images/s8.jpg'
+},{
+    id:9,
+    img:'/worksImages/work_social_images/s9.jpg'
+},{
+    id:10,
+    img:'/worksImages/work_social_images/s10.jpg'
+},{
+    id:11,
+    img:'/worksImages/work_social_images/s11.jpg'
+}] 
+export const socialSlid4= [{
+    id: 1,
+    img: "/worksImages/work_social_images/s1.jpg"
+},
+{
+    id:2,
+    img:'/worksImages/work_social_images/s2.jpg'
+},
+{
+    id:3,
+    img:'/worksImages/work_social_images/s3.jpg'
+},{
+    id:4,
+    img:'/worksImages/work_social_images/s4.jpg'
+},{
+    id:5,
+    img:'/worksImages/work_social_images/s5.jpg'
+},{
+    id:6,
+    img:'/worksImages/work_social_images/s6.jpg'
+},{
+    id:7,
+    img:'/worksImages/work_social_images/s7.jpg'
+},{
+    id:8,
+    img:'/worksImages/work_social_images/s8.jpg'
+},{
+    id:9,
+    img:'/worksImages/work_social_images/s9.jpg'
+},{
+    id:10,
+    img:'/worksImages/work_social_images/s10.jpg'
+},{
+    id:11,
+    img:'/worksImages/work_social_images/s11.jpg'
+}]
+export const socialSlid5 = [{
     id: 1,
     img: "/worksImages/work_social_images/s1.jpg"
 },
