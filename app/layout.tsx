4c7110bc2553613516ba32d01ad15abcb12979c1
Hamadalishah <hammadalis459@gmail.com>
2025-04-15 21:33:47 +0500

    make the smooth scroll effect using lenis

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import "./globals.css"
 import type { Metadata } from "next"
 import { GeistSans, GeistMono } from 'geist/font'
+import { PageProvider } from "./smoothScrollComponent/scrollprovider"
 
 export const metadata: Metadata = {
   title: "Mars Black",
@@ -16,7 +17,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${GeistSans.variable} ${GeistMono.variable} antialiased bg-black` }>
-        {children}
+        <PageProvider>   {children}</PageProvider>
+      
       </body>
     </html>
   )
