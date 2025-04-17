9e0e57bef547fde92580bd9275f449ada6958329
Hamadalishah <hammadalis459@gmail.com>
2025-04-17 20:06:14 +0500

    make the some changes in social.tsx

--- app/work/Components/social.tsx
+++ app/work/Components/social.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Social() {
   return (
-    <div className='text-center text-[400px] font-[900px]'>Social.</div>
+    <div className='text-center text-white text-[400px] font-[900px]'>Social.</div>
   )
 }
